cee24b2749027bd1fe711261ca54d29e1e910c34
rottenhyena <matteo9679@hotmail.com>
2022-12-14 12:51:36 +0100

    Update1

--- script.js
+++ script.js
@@ -1,30 +1,5 @@
-document.querySelector("#red").addEventListener("click", function(e){
-    let color = e.target.id
-    body.style.backgroundColor = color
-})
+document.querySelector("#green").addEventListener("click", function(e) { 
+    let color = e.target.id //let dichiarazione della varibile in js
 
-
-document.querySelector("#green").addEventListener("click", function(e){
-    let color = e.target.id
-    body.style.backgroundColor = color
-})
-
-document.querySelector("#blue").addEventListener("click", function(e){
-    let color = e.target.id
-    body.style.backgroundColor = color
-})
-
-document.querySelector("#green2").addEventListener("click", function(e){
-    let color = e.target.id
-    body.style.backgroundColor = color
-})
-
-document.querySelector("#viola").addEventListener("click", function(e){
-    let color = e.target.id
-    body.style.backgroundColor = color
-})
-
-document.querySelector("#giallo").addEventListener("click", function(e){
-    let color = e.target.id
     body.style.backgroundColor = color
 })
