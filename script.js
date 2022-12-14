07cf3cd00804eb7051d3ecf6572f5e02e7a2bbd9
Botrek98 <pietrantoniobaronissi@gmail.com>
2022-12-14 12:56:59 +0100

    update2

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-document.querySelector("#green").addEventListener("click", function(e) { 
-    let color = e.target.id //let dichiarazione della varibile in js
+document.querySelector("#red").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
 
+
+document.querySelector("#green").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+document.querySelector("#blue").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+document.querySelector("#green2").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+document.querySelector("#viola").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+document.querySelector("#giallo").addEventListener("click", function(e){
+    let color = e.target.id
     body.style.backgroundColor = color
 })
